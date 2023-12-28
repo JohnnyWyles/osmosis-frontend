8ed2a46aa21076c436d26074f75969d4a9f22ffa
JohnnyWyles <97029546+JohnnyWyles@users.noreply.github.com>
2023-12-28 11:05:03 +0000

    Update Pool Creation Fee
    Changed according to https://www.mintscan.io/osmosis/proposals/699

--- packages/web/components/complex/pool/create/index.ts
+++ packages/web/components/complex/pool/create/index.ts
@@ -3,4 +3,4 @@ export * from "./step2-add-liquidity";
 export * from "./step3-confirm";
 export * from "./types";
 
-export const POOL_CREATION_FEE = "100 OSMO";
+export const POOL_CREATION_FEE = "100 USDC";
